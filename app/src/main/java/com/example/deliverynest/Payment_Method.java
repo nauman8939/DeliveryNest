3cfa9d4c239c4ef7698292d7479281993055de7a
nauman8939 <naumanpathan98239@gmail.com>
2023-03-02 16:39:19 +0530

    order summary+ payment page

--- app/src/main/java/com/example/deliverynest/Payment_Method.java
+++ app/src/main/java/com/example/deliverynest/Payment_Method.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Payment_Method extends AppCompatActivity {
+public class Payment_Method extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
